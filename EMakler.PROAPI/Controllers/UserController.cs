1eae3e1f5c1268919362edc39e169be946f47d87
user <user@DESKTOP-DDOO6UB>
2024-05-25 14:39:35 +0400

    Create UserController

---
+++ EMakler.PROAPI/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMakler.PROAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
