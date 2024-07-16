df38f0e9ea7539d5038879ddff9bd687685b4a79
user <user@DESKTOP-DDOO6UB>
2024-07-16 12:22:28 +0400

    Add UserValidator

---
+++ BusinessLayer/Validators/UserValidator.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Validators
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.ContactNumber)
+                .NotEmpty()
+                .WithMessage("Phone number cannot be empty");
+
+            RuleFor(x => x.UserMail)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("Email is incorrect");
+
+            RuleFor(x => x.UserPassword)
+                .NotEmpty()
+                .MinimumLength(8).When(x => x.UserPassword.Length > 0)
+                .WithMessage("Password must be longer than 8 characters");
+
+
+                     
+        }
+    }
+}
